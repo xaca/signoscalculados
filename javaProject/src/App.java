b0f6b124515012ed587087123bff76adfd55cbe4
Sebastian171962 <88748607+Sebastian171962@users.noreply.github.com>
2021-10-23 13:35:34 -0500

    Update App.java

--- javaProject/src/App.java
+++ javaProject/src/App.java
@@ -2,7 +2,21 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.text.DecimalFormat;
+public class App {
 
+    public static void main(String[] args) throws Exception {                           //Inicio
+
+        Scanner user = new Scanner(System.in);
+        String name;
+        int eleccion1 = 0;
+
+        System.out.println("           __");
+        System.out.println("          /\\ \\");
+        System.out.println("         / /\\ \\       Hola");
+        System.out.println("        / /__\\ \\      ¿Como deseas que te llame?");
+        System.out.println("        \\/____\\/");
+        System.out.println("");
+        name = user.nextLine();
 
 
 
