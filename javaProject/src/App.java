518c20fc8acc62b451e752ad7eefcf84fedfcba9
Xaca Rana <and1res@hotmail.com>
2021-11-20 09:24:35 -0500

    Update App.java
    Leer el comentario 😺

--- javaProject/src/App.java
+++ javaProject/src/App.java
@@ -2,7 +2,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 import java.text.DecimalFormat;
-
+//Muy bien, recuerden no subir los .class al repositorio, configuren un .gitignore para omitir estos archivos
 public class App {
 
     public static void main(String[] args) throws Exception {                           //Inicio
